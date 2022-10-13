4f20b7f23389f081ca423021685f72a7e4ab7d4a
--add <--add>
2022-10-13 09:31:17 -0300

    ok

--- cypress.config.js
+++ cypress.config.js
@@ -1,6 +1,7 @@
 const { defineConfig } = require('cypress')
 
 module.exports = defineConfig({
+  projectId: "gmzveu",
   viewportWidth: 1000,
   viewportHeight: 660,
   urlBase: 'https://buger-eats-qa.vercel.app',
